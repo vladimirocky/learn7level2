657613afda9faef329cd063650765077cd994185
Ken Karlson <kenkarlson@mail.ru>
2022-04-06 11:16:18 +0300

    add list

---
+++ list/main.js
@@ -0,0 +1,64 @@
+'use strict';
+
+const todoInput = document.querySelector('.todoInput');
+const buttonTodo = document.querySelector('.button-todo');
+const list = document.querySelector('.list');
+
+let todoList = [];
+if (localStorage.getItem('todo')) {
+  todoList = JSON.parse(localStorage.getItem('todo'));
+  displayMessages();
+}
+
+buttonTodo.addEventListener('click', () => {
+  let newTodo = {
+    todo: todoInput.value,
+    checked: false,
+    important: false
+  };
+  todoList.push(newTodo);
+  displayMessages();
+  todoInput.value = '';
+  localStorage.setItem('todo', JSON.stringify(todoList));
+});
+
+function displayMessages() {
+  let displayMessage = '';
+  todoList.forEach(function (item, i) {
+    displayMessage +=
+      `
+    <li>
+    <input type='checkbox' id='item_${i}' ${item.checked ? 'checked' : ''}>
+    <label for='item_${i}' class="${item.important ? 'alert': ''}">${item.todo}</label>
+    </li>
+    `;
+    list.innerHTML = displayMessage;
+  });
+}
+
+list.addEventListener('change', (event) => {
+  let valueLabel = list.querySelector('[for=' + event.target.getAttribute('id') + ']').innerHTML;
+  todoList.forEach(function (item) {
+    if (item.todo === valueLabel) {
+      item.checked = !item.checked;
+      localStorage.setItem('todo', JSON.stringify(todoList));
+    }
+  });
+});
+list.addEventListener('contextmenu', function (event) {
+  event.preventDefault();
+  //console.log(item.todo);
+  todoList.forEach(function (item) {
+    if (item.list != event.target.innerHTML) {
+
+      item.important = !item.important;
+      displayMessages();
+      localStorage.setItem('todo', JSON.stringify(todoList));
+    } else {
+      // item.important = !item.important;
+      // displayMessages();
+      // console.log('test');
+      // console.log(item.important);
+    }
+  });
+});
