5b02a0cc7477449f8a8acbe017280e15fc7440a1
Timur Markovnikov <gadzhibekovtimur@gmail.com>
2022-04-21 20:59:39 +0300

    изменил функции таска

--- My page/Интерактивный список/script.js
+++ My page/Интерактивный список/script.js
@@ -1,35 +1,67 @@
+// Получаем данные из DOM
 let addMessage = document.querySelector('.message'),
     addButton = document.querySelector('.add');
     todo = document.querySelector('.todo');
-
+// Массив для объекта
 let todoList = [];
 
+// Вешаем на кнопку событие 
 addButton.addEventListener('click', function(){
-    
+    // создаём объект для тасков.
         let newTodo = {
             todo: addMessage.value,
             checked: false,
             important: false
         }
-    
+    // пушим в массив новый объект
         todoList.push(newTodo);
+        // выгружаем вёрстку на страницу
         displayMessage();
+        // Чистим инпут
+        addMessage.value = "";
 })
 
 function displayMessage() {
-    
+    // прекращаем функцию, если пустой инпут
+    if (!addMessage.value) return;
     let displayMessage = "";
-
+    // чистим вёрстку, если оюъектов нет в массиве при удалении.
+    if (todoList.length === 0) todo.innerHTML = '';
+    // перебираем таски, выгружаем на страницу
     todoList.forEach(function(item, i) {
         displayMessage += `
         <li>
             <input type = 'checkbox' id = 'item_${i}' ${item.checked ? 'checked' : ''}>
-            <label for = 'item_${i}'>${item.todo}</label>
-            <button id = id = 'item_${i}'>Удалить</button>
+            <label for = 'item_${i}' class = "${item.important ? 'important' : ''}">${item.todo}</label>
         </li>
         `;
         todo.innerHTML = displayMessage;
-        message.value = ""; 
+        
     });
 
 }
+// вешаем событие удаления тасков..
+todo.addEventListener('contextmenu', function(event){
+    event.preventDefault();
+    todoList.forEach(function(item, i) {
+
+        if (event.ctrlKey) {
+            todoList.splice(i, 1);
+        } else {
+            item.important = !item.important;
+        }
+    })
+})
+
+// Первая проблема #1
+
+// Таски удаляются, только прогружается новая вёрстка, после добавления нового таска
+// не знаю как это исправить
+
+// Проблема вторая #2
+
+// Не смог реализовать кнопку для изменения таска, не понимаю покак как
+// В перспективе не понимаю, как реализовать кнопку для удаления так, чтобы 
+// кнопка прогружалась вместе с новым таском, и была закреплена за ним...
+
+// HELP PLEASE
